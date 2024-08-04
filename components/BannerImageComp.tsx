5740dc111469ed92f7328a49bca039450f568186
tachyonok <tachyonparticle123@gmail.com>
2024-08-04 23:59:33 +0530

    colors

--- components/BannerImageComp.tsx
+++ components/BannerImageComp.tsx
@@ -19,7 +19,7 @@ const BannerImageComp = ({
 }: BannerImageProps) => {
   return (
     <div
-      className=" relative p-4  m-3 text-white bg-cover bg-center shadow-lg h-auto md:h-80 flex justify-between gap-4 items-center "
+      className=" relative p-4  m-3 text-white bg-cover bg-center shadow-lg h-auto md:h-80 flex justify-between gap-4 items-center rounded-lg "
       style={{
         backgroundImage: `url(${background})`,
       }}
@@ -28,13 +28,13 @@ const BannerImageComp = ({
         <MdEdit onClick={onEdit} />
       </button>
       <div className="ml-4 md:ml-6 flex-1 min-w-0">
-        <h2 className="text-lg md:text-3xl font-bold mb-3 text-white bg-black bg-opacity-50 px-3 py-1">
+        <h2 className="text-lg md:text-3xl font-bold mb-3 text-white bg-black bg-opacity-50 px-3 py-1 rounded-lg">
           {title}
         </h2>
-        <p className="text-xs md:text-lg text-white bg-black bg-opacity-75 py-1 px-3 mb-3">
+        <p className="text-xs md:text-lg text-white bg-black bg-opacity-75 py-1 px-3 mb-3 rounded-lg">
           {description}
         </p>
-        <button className="bg-black text-xs md:text-base text-white font-semibold px-4 py-2 rounded hover:bg-gray-700 transition">
+        <button className="bg-white text-xs md:text-base text-black font-semibold px-4 py-2 rounded hover:bg-gray-700 transition">
           {cta}
         </button>
       </div>
@@ -43,7 +43,7 @@ const BannerImageComp = ({
         <img
           src={image}
           alt=""
-          className="w-24 h-48   object-cover border-2 border-gray-300 shadow-lg "
+          className="w-24 h-48   object-cover border-2 border-gray-300 shadow-lg rounded-lg "
         />
       </div>
     </div>
